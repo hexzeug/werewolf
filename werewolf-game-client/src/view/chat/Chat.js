96b36467acb8267d3056aaffdd42a9219f6588bb
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-05-21 22:30:41 +0200

    added scroll down button for Chat component * missing explanatory comments * missing better variable naming

--- werewolf-game-client/src/view/chat/Chat.js
+++ werewolf-game-client/src/view/chat/Chat.js
@@ -4,42 +4,64 @@ import './Chat.css';
 import { useChat } from '../../model/chat';
 import { useTranslation } from 'react-i18next';
 
+const TIMEOUT_MS = 100;
+
 const Chat = () => {
   const { t } = useTranslation();
   const [messages, sendMessage] = useChat();
-  // scrolling on new message
+  const [atBottomState, setAtBottomState] = useState(true);
+  const atBottomRef = useRef(atBottomState);
+  const [newMessages, setNewMessages] = useState(false);
+  const setAtBottom = useCallback((newState) => {
+    if (newState) {
+      setNewMessages(false);
+    }
+    atBottomRef.current = newState;
+    setAtBottomState(newState);
+  }, []);
   const refScrollContainer = useRef(null);
-  const refScrollBottom = useRef(null);
-  const oldScrollTopMax = useRef(null);
   const scrollTimeout = useRef(null);
-  const resetScrollTimeout = () => {
+  const recalculateAtBottom = useCallback(() => {
+    const scrollTopMax =
+      refScrollContainer.current.scrollHeight -
+      refScrollContainer.current.clientHeight;
+    const scrollTop = refScrollContainer.current.scrollTop;
+    setAtBottom(scrollTop >= scrollTopMax);
+  }, [setAtBottom]);
+  const resetScrollTimeout = useCallback(() => {
     if (scrollTimeout.current) {
       clearTimeout(scrollTimeout.current);
     }
     scrollTimeout.current = setTimeout(() => {
       scrollTimeout.current = null;
-    }, 100);
-  };
+      recalculateAtBottom();
+    }, TIMEOUT_MS);
+  }, [recalculateAtBottom]);
+  const scrollToBottom = useCallback(() => {
+    const scrollTopMax =
+      refScrollContainer.current.scrollHeight -
+      refScrollContainer.current.clientHeight;
+    refScrollContainer.current.scroll({
+      top: scrollTopMax,
+      behavior: 'smooth',
+    });
+    resetScrollTimeout();
+    setAtBottom(true);
+  }, [resetScrollTimeout, setAtBottom]);
   const handleScroll = useCallback(() => {
     if (scrollTimeout.current) {
       resetScrollTimeout();
+    } else {
+      recalculateAtBottom();
     }
-  }, []);
+  }, [resetScrollTimeout, recalculateAtBottom]);
   useEffect(() => {
-    const container = refScrollContainer.current;
-    if (
-      !oldScrollTopMax.current ||
-      scrollTimeout.current ||
-      container.scrollTop >= oldScrollTopMax.current
-    ) {
-      refScrollBottom.current.scrollIntoView({
-        block: 'start',
-        behavior: 'smooth',
-      });
-      resetScrollTimeout();
+    if (atBottomRef.current) {
+      scrollToBottom();
+    } else {
+      setNewMessages(true);
     }
-    oldScrollTopMax.current = container.scrollHeight - container.clientHeight;
-  }, [messages]);
+  }, [messages, scrollToBottom]);
   // send messages
   const [inputMsg, setInputMsg] = useState('');
   const handleInputChange = useCallback((e) => setInputMsg(e.target.value), []);
@@ -55,17 +77,25 @@ const Chat = () => {
   );
   return (
     <div className="Chat">
-      <div
-        className="Chat__scroll-container"
-        ref={refScrollContainer}
-        onScroll={handleScroll}
-      >
-        <div className="Chat__body">
-          {messages.map((message, i) => (
-            <ChatMessage message={message} key={i} />
-          ))}
+      <div className="Chat__main-container">
+        <div
+          className="Chat__scroll-container"
+          ref={refScrollContainer}
+          onScroll={handleScroll}
+        >
+          <div className="Chat__body">
+            {messages.map((message, i) => (
+              <ChatMessage message={message} key={i} />
+            ))}
+          </div>
         </div>
-        <div className="Chat__scroll-bottom" ref={refScrollBottom} />
+        {!atBottomState && (
+          <button
+            className="Chat__scroll-down button"
+            data-new-messages={newMessages || null}
+            onClick={scrollToBottom}
+          />
+        )}
       </div>
       <hr />
       <form className="Chat__form" onSubmit={handleSubmit}>
