b698e5556e4959e12148f133631236fa86218c3e
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-06-03 12:16:20 +0200

    forgot updating narrator tests

--- werewolf-game-client/src/view/narrator/Narrator.spec.js
+++ werewolf-game-client/src/view/narrator/Narrator.spec.js
@@ -6,14 +6,17 @@ jest.mock('../../model/narrator');
 
 describe('narrator component', () => {
   it('should render the given text', () => {
-    useNarrator.mockReturnValue(['test-narration', { current: null }]);
+    useNarrator.mockReturnValue([
+      { text: 'test-narration' },
+      { current: null },
+    ]);
     render(<Narrator />);
     expect(screen.getByText('test-narration')).toBeInTheDocument();
   });
 
   it('should pass dom element to ref', () => {
     const ref = { current: null };
-    useNarrator.mockReturnValue(['test-narration', ref]);
+    useNarrator.mockReturnValue([{ text: 'test-narration' }, ref]);
     render(<Narrator />);
     expect(ref.current).toBe(screen.getByText('test-narration'));
   });
