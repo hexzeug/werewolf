39543f0a6820d29708c0f4fd1d59efc00267938f
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-06-03 08:18:59 +0200

    tests for Narrator component

---
+++ werewolf-game-client/src/view/narrator/Narrator.spec.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import Narrator from './Narrator';
+import { useNarrator } from '../../model/narrator';
+
+jest.mock('../../model/narrator');
+
+describe('narrator component', () => {
+  it('should render the given text', () => {
+    useNarrator.mockReturnValue(['test-narration', { current: null }]);
+    render(<Narrator />);
+    expect(screen.getByText('test-narration')).toBeInTheDocument();
+  });
+
+  it('should pass dom element to ref', () => {
+    const ref = { current: null };
+    useNarrator.mockReturnValue(['test-narration', ref]);
+    render(<Narrator />);
+    expect(ref.current).toBe(screen.getByText('test-narration'));
+  });
+});
