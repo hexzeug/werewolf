e6f463b435e72dbdf7fdd5bff8b8d7a0246be912
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-06-19 15:40:32 +0200

    documented the connection lifecycle events

--- werewolf-backend/src/main/java/com/hexszeug/werewolf/game/events/connections/ConnectedEvent.java
+++ werewolf-backend/src/main/java/com/hexszeug/werewolf/game/events/connections/ConnectedEvent.java
@@ -1,3 +1,6 @@
 package com.hexszeug.werewolf.game.events.connections;
 
+/**
+ * Is fired on creation of a new {@link com.hexszeug.werewolf.game.connections.Connection Connection}.
+ */
 public interface ConnectedEvent extends ConnectionLifecycleEvent {}
--- werewolf-backend/src/main/java/com/hexszeug/werewolf/game/events/connections/DisconnectingEvent.java
+++ werewolf-backend/src/main/java/com/hexszeug/werewolf/game/events/connections/DisconnectingEvent.java
@@ -1,5 +1,15 @@
 package com.hexszeug.werewolf.game.events.connections;
 
+/**
+ * Is fired before a {@link com.hexszeug.werewolf.game.connections.Connection Connection} closes.
+ * At that state, the closing cannot be canceled,
+ * but the connection can still be used to send information about the connection closing to the client.
+ * This event may contain an exception as the cause of the closure.
+ */
 public interface DisconnectingEvent extends ConnectionLifecycleEvent {
+    /**
+     * Returns the cause of the closure or {@code null} if there was no cause specified.
+     * @return the cause
+     */
     Throwable getCause();
 }
