8f649b546e15369dd0e70b7420d5433de78735a5
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-06-27 21:24:02 +0200

    added post to /seer endpoint (with docs)

--- werewolf-backend/src/main/java/com/hexszeug/werewolf/game/controller/SeerController.java
+++ werewolf-backend/src/main/java/com/hexszeug/werewolf/game/controller/SeerController.java
@@ -1,13 +1,16 @@
 package com.hexszeug.werewolf.game.controller;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.hexszeug.werewolf.game.controller.exceptions.BadRequestException;
 import com.hexszeug.werewolf.game.controller.exceptions.ForbiddenException;
 import com.hexszeug.werewolf.game.model.player.Player;
 import com.hexszeug.werewolf.game.model.player.role.Role;
 import com.hexszeug.werewolf.game.model.village.Village;
+import com.hexszeug.werewolf.game.model.village.phase.Phase;
 import lombok.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -40,32 +43,18 @@ public class SeerController {
         if (player.getRole() != Role.SEER) {
             throw new ForbiddenException("You must be seer.");
         }
-        Map<?, ?> seerHistory;
-        try {
-            seerHistory = player.get(KEY_SEER_HISTORY, Map.class);
-        } catch (ClassCastException ex) {
-            throw new IllegalStateException("Seer history is not a map.", ex);
-        }
-        if (seerHistory == null || seerHistory.isEmpty()) {
-            return Collections.emptyMap();
-        }
-        return seerHistory
+        return getSeerHistory(player)
                 .entrySet()
                 .stream()
                 .map(entry -> {
-                    if (!(entry.getKey() instanceof String playerId)) {
-                        throw new IllegalStateException("Seer history contains non string key.");
-                    }
-                    if (!(entry.getValue() instanceof Integer IGTime)) {
-                        throw new IllegalStateException("Seer history contains non integer value.");
-                    }
+                    String playerId = entry.getKey();
                     Player seen = village.getPlayerById(playerId);
                     if (seen == null) {
                         throw new IllegalStateException("Seer history contains non existing player.");
                     }
-                    return new AbstractMap.SimpleImmutableEntry<>(
+                    return (Map.Entry<String, SeenInfo>) new AbstractMap.SimpleImmutableEntry<>(
                             playerId,
-                            new SeenInfo(seen.getRole(), IGTime)
+                            new SeenInfo(seen.getRole(), entry.getValue())
                     );
                 })
                 .collect(Collectors.toMap(
@@ -74,9 +63,80 @@ public class SeerController {
                 ));
     }
 
+    /**
+     * @apiNote
+     * <b>Permissions:</b>
+     * <ol>
+     *     <li>player is a seer</li>
+     *     <li>seer phase is currently active</li>
+     * </ol>
+     * <b>Request:</b>
+     * <pre>
+     * {@link String} (player id)
+     * </pre>
+     * <b>Response:</b>
+     * <p>
+     *     {@code 301 CREATED}
+     * </p>
+     * <b>Effects:</b>
+     * <ul>
+     *     <li>pushes player to list of seen people (doesn't check if already seen)</li>
+     *     <li>continues narration</li>
+     * </ul>
+     * @throws ForbiddenException if the permissions are not fulfilled
+     * @throws BadRequestException if passed string is not a valid player id of the village
+     */
+    @PostMapping(value = "/seer", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void handlePost(@RequestBody TextNode playerIdTextNode, Player player, Village village) {
+        String playerId = playerIdTextNode.asText();
+        System.out.println(playerId);
+        if (player.getRole() != Role.SEER) {
+            throw new ForbiddenException("You must be seer.");
+        }
+        if (village.getCurrentPhase() != Phase.SEER) {
+            throw new ForbiddenException("It must be seer phase.");
+        }
+        if (village.getPlayerById(playerId) == null) {
+            throw new BadRequestException("Player %s is not in the village.".formatted(playerId));
+        }
+        Map<String, Integer> seerHistory = new HashMap<>(getSeerHistory(player));
+        seerHistory.put(playerId, village.getIGTime());
+        player.set(KEY_SEER_HISTORY, seerHistory);
+        //TODO continue narration
+    }
+
+    private Map<String, Integer> getSeerHistory(Player player) {
+        Map<?, ?> uncheckedSeerHistory;
+        try {
+            uncheckedSeerHistory = player.get(KEY_SEER_HISTORY, Map.class);
+        } catch (ClassCastException ex) {
+            throw new IllegalStateException("Seer history is not a map.", ex);
+        }
+        if (uncheckedSeerHistory == null) {
+            return Collections.emptyMap();
+        }
+        Map<String, Integer> seerHistory = new HashMap<>();
+        uncheckedSeerHistory.forEach((k, v) -> {
+            if (!(k instanceof String key)) {
+                throw new IllegalStateException("Seer history contains non string key.");
+            }
+            if (!(v instanceof Integer value)) {
+                throw new IllegalStateException("Seer history contains non integer value.");
+            }
+            seerHistory.put(key, value);
+        });
+        return seerHistory;
+    }
+
     @Value
     private static class SeenInfo {
         Role role;
         int igtime;
+    }
+
+    @Value
+    private static class PlayerToBeSeenInfo {
+        @JsonValue
+        String playerId;
     }
 }
