8cb9fd59eabc7ec25871c07d553c1ef88404eed4
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-06-30 16:05:32 +0200

    added /hunter endpoint

---
+++ werewolf-backend/src/main/java/com/hexszeug/werewolf/game/controller/HunterController.java
@@ -0,0 +1,61 @@
+package com.hexszeug.werewolf.game.controller;
+
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.hexszeug.werewolf.game.controller.exceptions.BadRequestException;
+import com.hexszeug.werewolf.game.controller.exceptions.ForbiddenException;
+import com.hexszeug.werewolf.game.model.player.Player;
+import com.hexszeug.werewolf.game.model.player.role.Role;
+import com.hexszeug.werewolf.game.model.village.Village;
+import com.hexszeug.werewolf.game.model.village.phase.Phase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class HunterController {
+
+    /**
+     * @apiNote
+     * <b>Permissions:</b>
+     * <ol>
+     *     <li>player is a hunter</li>
+     *     <li>current phase is hunter phase</li>
+     * </ol>
+     * <b>Request:</b>
+     * <pre>
+     * {@link String} (player id)
+     * </pre>
+     * <b>Response:</b>
+     * <p>
+     *     {@code 201 CREATED}
+     * </p>
+     * <b>Effects:</b>
+     * <ol>
+     *     <li>kill the passed player</li>
+     *     <li>continue narration</li>
+     * </ol>
+     * @throws ForbiddenException if permissions are not fulfilled
+     * @throws BadRequestException if the player does not exist or is already dead
+     */
+    @PostMapping(value = "/hunter", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void handlePostHunter(@RequestBody TextNode playerIdTextNode, Player player, Village village) {
+        if (player.getRole() != Role.HUNTER) {
+            throw new ForbiddenException("You must be a hunter.");
+        }
+        if (village.getCurrentPhase() != Phase.HUNTER) {
+            throw new ForbiddenException("The phase must be the hunter phase.");
+        }
+        String playerId = playerIdTextNode.asText();
+        Player target = village.getPlayerById(playerId);
+        if (target == null) {
+            throw new BadRequestException("Player %s does not exist in the village.".formatted(playerId));
+        }
+        if (!target.isAlive()) {
+            throw new BadRequestException("Player %s is already dead.".formatted(playerId));
+        }
+        //TODO kill player
+        //TODO continue narration
+    }
+}
