1b77ca31d17d8dcadae6ec2db1107b8e7f5d270f
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-06-30 16:59:21 +0200

    added PUT /accusation endpoint

--- werewolf-backend/src/main/java/com/hexszeug/werewolf/game/controller/CourtController.java
+++ werewolf-backend/src/main/java/com/hexszeug/werewolf/game/controller/CourtController.java
@@ -1,21 +1,29 @@
 package com.hexszeug.werewolf.game.controller;
 
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.hexszeug.werewolf.game.controller.exceptions.BadRequestException;
 import com.hexszeug.werewolf.game.controller.exceptions.ForbiddenException;
+import com.hexszeug.werewolf.game.events.connections.ServerSentEvent;
 import com.hexszeug.werewolf.game.model.player.Player;
 import com.hexszeug.werewolf.game.model.village.Village;
 import com.hexszeug.werewolf.game.model.village.phase.Phase;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class CourtController {
     public static final String KEY_ACCUSATION = "accusation";
+
+    private final ApplicationEventPublisher eventPublisher;
 
     /**
      * @apiNote
@@ -52,12 +60,68 @@ public class CourtController {
                 .toList();
     }
 
+    /**
+     * @apiNote
+     * <b>Permissions:</b>
+     * <ol>
+     *     <li>current phase is accusation</li>
+     *     <li>player is alive</li>
+     * </ol>
+     * <b>Request:</b>
+     * <pre>
+     * {@link String} (player id)
+     * </pre>
+     * <b>Response:</b>
+     * <p>
+     *     {@code 204 NO CONTENT}
+     * </p>
+     * <b>Effects:</b>
+     * <ol>
+     *     <li>set own accusation (if target player is already accused exception is thrown)</li>
+     *     <li>publishes {@link AccusationEvent}</li>
+     * </ol>
+     * @throws ForbiddenException if the permissions are not fulfilled
+     * @throws BadRequestException if the passed player does not exist of is dead or is already accused
+     */
+    @PutMapping(value = "/accusation", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handlePutAccusation(@RequestBody TextNode playerIdTextNode, Player player, Village village) {
+        if (village.getCurrentPhase() != Phase.ACCUSATION) {
+            throw new ForbiddenException("The phase must be accusation.");
+        }
+        if (!player.isAlive()) {
+            throw new ForbiddenException("You must be alive.");
+        }
+        String playerId = playerIdTextNode.asText();
+        Player accusation = village.getPlayerById(playerId);
+        if (accusation == null) {
+            throw new BadRequestException("Player %s does not exist in this village.".formatted(playerId));
+        }
+        if (!accusation.isAlive()) {
+            throw new BadRequestException("Player %s is already dead.".formatted(playerId));
+        }
+        if (village.getPlayerList().stream().anyMatch(p -> playerId.equals(getAccusation(p)))) {
+            throw new BadRequestException("Player %s is already accused.".formatted(playerId));
+        }
+        player.set(KEY_ACCUSATION, playerId);
+        eventPublisher.publishEvent(new AccusationEvent(
+                new AccusationInfo(player.getPlayerId(), playerId),
+                village.getVillageId()
+        ));
+    }
+
     private String getAccusation(Player player) {
         try {
             return player.get(KEY_ACCUSATION, String.class);
         } catch (ClassCastException ex) {
             throw new IllegalStateException("Accusation contains non string.", ex);
         }
+    }
+
+    @Value
+    public static class AccusationEvent implements ServerSentEvent<AccusationInfo> {
+        AccusationInfo payload;
+        String villageId;
     }
 
     @Value
