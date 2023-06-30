c95f00ebf2af2ae98174c676d082f61e782549c9
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-06-30 15:41:18 +0200

    added GET & POST /witch/poison endpoint

--- werewolf-backend/src/main/java/com/hexszeug/werewolf/game/controller/WitchController.java
+++ werewolf-backend/src/main/java/com/hexszeug/werewolf/game/controller/WitchController.java
@@ -1,6 +1,7 @@
 package com.hexszeug.werewolf.game.controller;
 
 import com.fasterxml.jackson.databind.node.BooleanNode;
+import com.fasterxml.jackson.databind.node.ValueNode;
 import com.hexszeug.werewolf.game.controller.exceptions.BadRequestException;
 import com.hexszeug.werewolf.game.controller.exceptions.ForbiddenException;
 import com.hexszeug.werewolf.game.model.player.Player;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/witch")
 public class WitchController {
-    private static final String KEY_HEALED = "witchHealed";
+    private static final String KEY_DID_HEAL = "witchDidHeal";
+    private static final String KEY_DID_POISON = "witchDidPoison";
+    public static final String KEY_WITCH_POISONED = "witchPoisoned";
 
     /**
      * @apiNote
@@ -24,7 +27,7 @@ public class WitchController {
      * </ul>
      * <b>Response:</b>
      * <pre>
-     * {@code boolean}
+     * {@code boolean} (true = heal potion left, false = potion already used)
      * </pre>
      * @throws ForbiddenException if the permissions are not fulfilled
      */
@@ -34,7 +37,7 @@ public class WitchController {
             throw new ForbiddenException("You must be a witch.");
         }
         // note the boolean is inverted now representing the existence of a heeling potion
-        return !getHealed(player);
+        return !didHeal(player);
     }
 
     /**
@@ -70,21 +73,107 @@ public class WitchController {
             throw new ForbiddenException("The phase must be witch heal.");
         }
         if (booleanNode.asBoolean()) {
-            if (getHealed(player)) {
+            if (didHeal(player)) {
                 throw new BadRequestException("The heal potion was already used.");
             }
             village.delete(WerewolfController.KEY_WEREWOLF_VICTIM);
-            player.set(KEY_HEALED, true);
+            player.set(KEY_DID_HEAL, true);
         }
         //TODO continue narration
     }
 
-    private boolean getHealed(Player player) {
+    /**
+     * @apiNote
+     * <b>Permissions:</b>
+     * <ul>
+     *     <li>player is a witch</li>
+     * </ul>
+     * <b>Response:</b>
+     * <pre>
+     * {@code boolean} (true = poison potion left, false = potion already used)
+     * </pre>
+     * @throws ForbiddenException if the permissions are not fulfilled
+     */
+    @GetMapping(value = "/poison", produces = MediaType.APPLICATION_JSON_VALUE)
+    public boolean handleGetPoison(Player player) {
+        if (player.getRole() != Role.WITCH) {
+            throw new ForbiddenException("You must be a witch.");
+        }
+        // note the boolean is inverted now representing the existence of a poison potion
+        return !didPoison(player);
+    }
+
+    /**
+     * @apiNote
+     * <b>Permissions:</b>
+     * <ol>
+     *     <li>player is a witch</li>
+     *     <li>current phase is witch heal</li>
+     * </ol>
+     * <b>Request:</b>
+     * <pre>
+     * {@link String} or {@code null} (player id)
+     * </pre>
+     * <b>Response:</b>
+     * <p>
+     *     {@code 204 NO CONTENT}
+     * </p>
+     * <b>Effects:</b>
+     * <ol>
+     *     <li>set witch poisoned to passed player id (or do nothing if {@code null})</li>
+     *     <li>continue narration</li>
+     * </ol>
+     * @throws ForbiddenException if permissions are not fulfilled
+     * @throws BadRequestException if the body is not null and poison potion was already used
+     *                             or if the player does not exist
+     *                             or if the player is already dead
+     *                             or if the body contains neither a {@link String} nor {@code null}
+     */
+    @PostMapping(value = "/poison", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handlePostPoison(@RequestBody ValueNode playerIdNode, Player player, Village village) {
+        if (player.getRole() != Role.WITCH) {
+            throw new ForbiddenException("You must be a witch.");
+        }
+        if (village.getCurrentPhase() != Phase.WITCH_POISON) {
+            throw new ForbiddenException("The phase must be witch poison.");
+        }
+        if (!playerIdNode.isNull() && !playerIdNode.isTextual()) {
+            throw new BadRequestException("The body must either contain a string or null.");
+        }
+        if (playerIdNode.isTextual()) {
+            String playerId = playerIdNode.asText();
+            Player poisoned = village.getPlayerById(playerId);
+            if (poisoned == null) {
+                throw new BadRequestException("Player %s does not exist in the village.".formatted(playerId));
+            }
+            if (didPoison(player)) {
+                throw new BadRequestException("The poison potion was already used.");
+            }
+            if (!poisoned.isAlive()) {
+                throw new BadRequestException("The targeted player %s is already dead.".formatted(playerId));
+            }
+            village.set(KEY_WITCH_POISONED, playerId);
+            player.set(KEY_DID_POISON, true);
+        }
+        //TODO continue narration
+    }
+
+    private boolean didHeal(Player player) {
         try {
-            Boolean heal = player.get(KEY_HEALED, Boolean.class);
+            Boolean heal = player.get(KEY_DID_HEAL, Boolean.class);
             return heal != null && heal;
         } catch (ClassCastException ex) {
-            throw new IllegalStateException("Witch heal contains non boolean.", ex);
+            throw new IllegalStateException("Witch did heal contains non boolean.", ex);
+        }
+    }
+
+    private boolean didPoison(Player player) {
+        try {
+            Boolean heal = player.get(KEY_DID_POISON, Boolean.class);
+            return heal != null && heal;
+        } catch (ClassCastException ex) {
+            throw new IllegalStateException("Witch did poison contains non boolean.", ex);
         }
     }
 }
