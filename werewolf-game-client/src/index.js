bb12e411516295b6270b49a05989031cad9e07ec
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-07-03 15:22:28 +0200

    added auth token url param

--- werewolf-game-client/src/index.js
+++ werewolf-game-client/src/index.js
@@ -5,6 +5,12 @@ import './index.css';
 import App from './view/App';
 import { enablePatches } from 'immer';
 
+// for debugging
+if (window.location.hash.length > 10) {
+  document.cookie = `wat=${window.location.hash.slice(1)}`;
+  window.location.hash = '';
+}
+
 enablePatches();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
