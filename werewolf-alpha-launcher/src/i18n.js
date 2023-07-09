37a03ba1328004405ca564ef5a9a3e1acc060e90
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-07-09 16:32:31 +0200

    added frontend sub-project

---
+++ werewolf-alpha-launcher/src/i18n.js
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+await i18n.use(initReactI18next).init({
+  debug: true,
+  lng: 'de',
+  interpolation: {
+    escapeValues: false,
+  },
+  resources: {
+    de: {
+      translation: {
+        'document.title': 'Werwolf Alpha Launcher',
+      },
+    },
+  },
+});
+
+i18n.on('languageChanged', (lng) => {
+  document.title = i18n.t('document.title');
+  document.documentElement.setAttribute('lang', lng);
+  document.documentElement.setAttribute('dir', i18n.dir());
+});
+
+export default i18n;
