09c55a0337a1986123182d57b144c82b152f92e5
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-07-11 15:39:11 +0200

    added feedback backend

--- werewolf-backend/src/main/java/com/hexszeug/werewolf/launcher/alpha/AlphaLauncherController.java
+++ werewolf-backend/src/main/java/com/hexszeug/werewolf/launcher/alpha/AlphaLauncherController.java
@@ -1,9 +1,16 @@
 package com.hexszeug.werewolf.launcher.alpha;
 
+import jakarta.servlet.ServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -11,6 +18,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AlphaLauncherController {
     private final GameManager gameManager;
+    private final Environment environment;
 
     @PostMapping("/game")
     public List<String> handlePostGame(@RequestParam("players") int playerCount) {
@@ -29,5 +37,20 @@ public class AlphaLauncherController {
             @CookieValue(value = "wat", required = false) String authToken
     ) {
         gameManager.setName(authToken, name);
+    }
+
+    @PostMapping(value = "/feedback", consumes = MediaType.TEXT_PLAIN_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void handlePostFeedback(@RequestBody String feedback, ServletRequest request) throws IOException {
+        if (!environment.getProperty("feedback", Boolean.TYPE, false)) return;
+        try (FileOutputStream fileOutputStream = new FileOutputStream("feedback.txt", true)) {
+            fileOutputStream.write(
+                    "%1$td.%1$tm.%1$tY %1$tT: (%2$s)\n%3$s\n\n\n".formatted(
+                            new Date(),
+                            request.getRemoteAddr(),
+                            feedback
+                    ).getBytes(StandardCharsets.UTF_8)
+            );
+        }
     }
 }
