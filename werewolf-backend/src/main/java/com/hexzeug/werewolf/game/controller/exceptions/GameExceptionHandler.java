64af3d93d0b65c240b1c80d4fb5f9ecfcb4a2ee6
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2023-07-12 22:13:16 +0200

    last mentions of 'hexszeug' package

--- werewolf-backend/src/main/java/com/hexzeug/werewolf/game/controller/exceptions/GameExceptionHandler.java
+++ werewolf-backend/src/main/java/com/hexzeug/werewolf/game/controller/exceptions/GameExceptionHandler.java
@@ -3,5 +3,5 @@ package com.hexzeug.werewolf.game.controller.exceptions;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@RestControllerAdvice("com.hexszeug.werewolf.game.controller")
+@RestControllerAdvice("com.hexzeug.werewolf.game.controller")
 public class GameExceptionHandler extends ResponseEntityExceptionHandler {}
